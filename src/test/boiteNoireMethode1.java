3bf2374f7bc074dce2717641add855ca7e74cea0
beepbop24 <81869542+beepbop24@users.noreply.github.com>
2022-04-03 13:15:51 -0400

    mise à jour des tests boîte noire

--- src/test/boiteNoireMethode1.java
+++ src/test/boiteNoireMethode1.java
@@ -1,6 +1,6 @@
 import currencyConverter.Currency;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 import java.util.ArrayList;
 
 public class boiteNoireMethode1 {
@@ -8,14 +8,11 @@ public class boiteNoireMethode1 {
     @Test
     void currency1() {
         String[] T = {"US Dollar", "allo"};
-        Double[] result = {1.0, 0.0};
+        Double[] result = {1.0, null};
         ArrayList<Currency> currencies = Currency.init();
 
         for (int i=0; i<T.length; i++){
-            Double newCurrency = currencyConverter.MainWindow.convert("US Dollar", T[i], currencies, 1.0);
-            // possiblement que on pourrait mettre en fait : assert que ça ne retourne pas une valeur >=0 dans le cas
-            // ou on veut une erreur. Parce que le but est que le test réussisse, pas fail. Ainsi, il faut mettre
-            //le comportement auquel on s'attend
+            Double newCurrency = currencyConverter.MainWindow.convert(T[i], "US Dollar", currencies, 1.0);
             assertEquals(newCurrency, result[i]);
         }
 
@@ -24,14 +21,11 @@ public class boiteNoireMethode1 {
     @Test
     void currency2() {
         String[] T = {"US Dollar", "allo"};
-        Double[] result = {1.0, 0.0};
+        Double[] result = {1.0, null};
         ArrayList<Currency> currencies = Currency.init();
 
         for (int i=0; i<T.length; i++){
-            Double newCurrency = currencyConverter.MainWindow.convert(T[i], "US Dollar", currencies, 1.0);
-            // possiblement que on pourrait mettre en fait : assert que ça ne retourne pas une valeur >=0 dans le cas
-            // ou on veut une erreur. Parce que le but est que le test réussisse, pas fail. Ainsi, il faut mettre
-            //le comportement auquel on s'attend
+            Double newCurrency = currencyConverter.MainWindow.convert("US Dollar", T[i], currencies, 1.0);
             assertEquals(newCurrency, result[i]);
         }
 
@@ -42,13 +36,15 @@ public class boiteNoireMethode1 {
         Double[] T = {-5000.0, -0.1, 0.0, 5000.0, 10000.0, 10000.1, 15000.0};
         ArrayList<Currency> currencies = Currency.init();
 
-        for (int i=0; i<T.length; i++){
+        for (int i=0; i<T.length; i++) {
             Double newCurrency = currencyConverter.MainWindow.convert("US Dollar", "US Dollar", currencies, T[i]);
-            // possiblement que on pourrait mettre en fait : assert que ça ne retourne pas une valeur >=0 dans le cas
-            // ou on veut une erreur. Parce que le but est que le test réussisse, pas fail. Ainsi, il faut mettre
-            //le comportement auquel on s'attend
-            assertEquals(newCurrency, T[i]);
-        }
+            if (T[i] > 10000.0 || T[i] < 0) {
+                assertNull(newCurrency);
+            }
 
+            else {
+                assertEquals(newCurrency, T[i]);
+            }
+        }
     }
 }
--- src/test/boiteNoireMethode2.java
+++ src/test/boiteNoireMethode2.java
@@ -1,6 +1,8 @@
 import currencyConverter.Currency;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
 import java.util.ArrayList;
 
 public class boiteNoireMethode2 {
@@ -11,9 +13,15 @@ public class boiteNoireMethode2 {
         for (int i=0; i<T.length; i++){
             Double newCurrency = currencyConverter.Currency.convert(T[i], 0.1);
 
-            Double price = T[i] * 0.1;
-            price = Math.round(price * 100d) / 100d;
-            assertEquals(newCurrency, price);
+            if (T[i] > 10000.0 || T[i] < 0) {
+                assertNull(newCurrency);
+            }
+
+            else {
+                double price = T[i] * 0.1;
+                price = Math.round(price * 100d) / 100d;
+                assertEquals(newCurrency, price);
+            }
         }
 
     }
